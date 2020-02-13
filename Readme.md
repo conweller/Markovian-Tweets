621767304eb24dca6dccfed3c7ff7b657b9a3590
Connor Onweller <onweller@udel.edu>
2020-02-12 22:33:39 -0500

    initial commit

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# Markovian Tweets
+
+A simple tweet generater. Takes a Twitter username and generates a tweet in the
+style of that Twitter user using Markov Chains.
---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(void)
+{
+  char input[] = " Exercitationem corporis placeat nam alias rerum. Sint libero a occaecati repudiandae voluptatem incidunt sed. Est eum sed nisi odit. Quisquam voluptatem est quis omnis nostrum quos quia.  Beatae dicta est maxime in et sed. Explicabo quibusdam sapiente pariatur ut pariatur soluta omnis. Corrupti tempora neque quidem tempore excepturi. Dolorem assumenda fugit expedita incidunt commodi quia.  Est illo ea aut tenetur ea eaque. Est adipisci quidem ea et illo temporibus similique. Aspernatur molestiae dolores ex alias numquam quidem. Sunt ad qui in similique nisi non impedit debitis.  Ut quia magnam quaerat nesciunt nostrum neque molestias. Expedita ab doloribus corporis dolores. Natus est aut dolor odio architecto.  Eius est vel quisquam eveniet molestiae doloremque. Suscipit eum ab sed odio voluptates. Error cumque et voluptas consequatur aut nobis tempore. Quia aut eum vero et et. Voluptatem alias eligendi quos in rerum laudantium pariatur.";
+
+  char tweet[288];
+  printf("Hello\n");
+  return 0;
+}
